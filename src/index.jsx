5a90db5b1c2feb19b1544f34ac367cf6551bd37b
deploy.sh <>
2018-04-09 16:30:06 +0100

    Moving things around on the page, better layout

--- src/index.jsx
+++ src/index.jsx
@@ -9,28 +9,34 @@ import GridItem from 'components/grid-item';
 function Application() {
     return (
         <div className="lr-o-site-wrap">
-            <Card className="lr-c-card--dark">
-                <Grid className="lr-o-grid--spaced">
-                    <GridItem className="lr-u-1/3@sm">
+            <Grid className="lr-o-grid--spaced lr-o-grid--match-height">
+                <GridItem className="lr-u-1/3@lg lr-u-1/2@md">
+                    <Card className="lr-c-card--dark lr-u-fill-height">
                         <img className="lr-u-fill-width" src={require('assets/images/horse_mask.jpg')}></img>
-                    </GridItem>
-                    <GridItem className="lr-u-1/3@sm">
-                        <h1>Hello</h1>
-                        <p>I am a Software Developer and Game Developer. I've worked as a Gameplay
-                            Programmer, and also as a Full Stack Web Developer. I've used C#, Java, and
-                            JavaScript, alongside Unity, XNA/MonoGame, Angularjs, React, and Dropwizard.</p>
-                    </GridItem>
-                    <GridItem className="lr-u-1/3@sm">
-                        <h3>Available For:</h3>
-                        <ul>
-                            <li>Front-End Web Development</li>
-                            <li>Back-End Web Development</li>
-                            <li>Gameplay Programming</li>
-                            <li>Unity Development</li>
-                        </ul>
-                    </GridItem>
-                </Grid>
-            </Card>
+                    </Card>
+                </GridItem>
+                <GridItem className="lr-u-2/3@lg lr-u-1/2@md">
+                    <Card className="lr-c-card--dark lr-u-fill-height">
+                        <Grid className="lr-o-grid--spaced">
+                            <GridItem className="lr-u-1/2@xs">
+                                <h1>Hello</h1>
+                                <p>I am a Software Developer and Game Developer. I've worked as a Gameplay
+                                    Programmer, and also as a Full Stack Web Developer. I've used C#, Java, and
+                                    JavaScript, alongside Unity, XNA/MonoGame, Angularjs, React, and Dropwizard.</p>
+                            </GridItem>
+                            <GridItem className="lr-u-1/2@xs">
+                                <h3>Available For:</h3>
+                                <ul>
+                                    <li>Front-End Web Development</li>
+                                    <li>Back-End Web Development</li>
+                                    <li>Gameplay Programming</li>
+                                    <li>Unity Development</li>
+                                </ul>
+                            </GridItem>
+                        </Grid>
+                    </Card>
+                </GridItem>
+            </Grid>
         </div>
     );
 }
