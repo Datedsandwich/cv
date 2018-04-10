dc7bea6db30044ecaf63b73548a916d37b45ea89
deploy.sh <>
2018-04-10 15:51:34 +0100

    Playing around with layout

--- src/index.jsx
+++ src/index.jsx
@@ -11,31 +11,38 @@ function Application() {
         <div>
             <Navbar/>
             <div className="lr-o-site-wrap">
-                <Grid.Wrapper className="lr-o-grid--spaced lr-o-grid--match-height">
-                    <Grid.Item className="lr-u-1/3@lg lr-u-1/2@md">
-                        <Card className="lr-c-card--dark lr-u-fill-height">
-                            <img className="lr-u-fill-width" src={require('assets/images/horse_mask.jpg')}></img>
-                        </Card>
-                    </Grid.Item>
-                    <Grid.Item className="lr-u-2/3@lg lr-u-1/2@md">
-                        <Card className="lr-c-card--dark lr-u-fill-height">
+                <Grid.Wrapper className="lr-o-grid--spaced">
+                    <Grid.Item>
+                        <Card className="lr-c-card--grey">
                             <Grid.Wrapper className="lr-o-grid--spaced">
-                                <Grid.Item className="lr-u-1/2@xs">
-                                    <h1>Hello</h1>
-                                    <p>I am a Software Developer and Game Developer. I've worked as a Gameplay
-                                        Programmer, and also as a Full Stack Web Developer. I've used C#, Java, and
-                                        JavaScript, alongside Unity, XNA/MonoGame, Angularjs, React, and Dropwizard.</p>
+                                <Grid.Item className="lr-u-1/3@lg lr-u-1/2@md">
+                                    <img className="lr-u-fill-width" src={require('assets/images/horse_mask.jpg')}></img>
                                 </Grid.Item>
-                                <Grid.Item className="lr-u-1/2@xs">
-                                    <h3>Available For:</h3>
-                                    <ul className="lr-o-list lr-o-list--bare">
-                                        <li>Front-End Web Development</li>
-                                        <li>Back-End Web Development</li>
-                                        <li>Gameplay Programming</li>
-                                        <li>Unity Development</li>
-                                    </ul>
+                                <Grid.Item className="lr-u-2/3@lg lr-u-1/2@md lr-u-soft-left">
+                                    <Grid.Wrapper className="lr-o-grid--spaced">
+                                        <Grid.Item>
+                                            <h2>Luke Robinson</h2>
+                                            <h3>Game and Software Developer</h3>
+                                        </Grid.Item>
+                                        <Grid.Item>
+                                            <h4>Phone:</h4>
+                                            <p>07570809503</p>
+                                        </Grid.Item>
+                                        <Grid.Item>
+                                            <h4>Email:</h4>
+                                            <p>robinsonlm@live.co.uk</p>
+                                        </Grid.Item>
+                                    </Grid.Wrapper>
                                 </Grid.Item>
                             </Grid.Wrapper>
+                        </Card>
+                    </Grid.Item>
+                    <Grid.Item>
+                        <Card className="lr-c-card--grey lr-u-soft lr-u-text-align-center">
+                            <h1>Hello!</h1>
+                            <p>I am a Software Developer and Game Developer. I've worked as a Gameplay
+                                Programmer, and also as a Full Stack Web Developer. I've used C#, Java, and
+                                JavaScript, alongside Unity, XNA/MonoGame, Angularjs, React, and Dropwizard.</p>
                         </Card>
                     </Grid.Item>
                 </Grid.Wrapper>
