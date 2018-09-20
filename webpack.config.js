04df8efc375357e073267dcbfa5dda639cb067e3
Luke Robinson <robinsonlm@live.co.uk>
2018-09-20 19:15:21 +0100

    Updated webpack config, added build to precommit hook

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,81 @@
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-    entry: ["./src/index.jsx"],
-    output: {
-        path: path.resolve(__dirname, "docs"),
-        filename: "[name].js"
-    },
-    devtool: 'source-map',
-    devServer: {
-        contentBase: "./docs"
-    },
-    resolve: {
-        modules: [
-            'src', 'node_modules'
-        ],
-        extensions: ['.js', '.jsx', '.scss']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            }, {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader",
-                        options: {
-                            minimize: true
-                        }
-                    }
-                ]
-            }, {
-                test: /\.(s*)css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-            }, {
-                test: /\.(jpe?g|png|gif)$/i,
-                exclude: /node_modules/,
-                loader: 'file-loader?name=[name].[hash].[ext]'
-            }
-        ]
-    },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                styles: {
-                    name: 'styles',
-                    test: /\.(s*)css$/,
-                    chunks: 'all',
-                    enforce: true
-                }
-            }
+  entry: ['./src/index.jsx'],
+  output: {
+    path: path.resolve(__dirname, 'docs'),
+    filename: '[name].js'
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './docs'
+  },
+  resolve: {
+    modules: ['src', 'node_modules'],
+    extensions: ['.js', '.jsx', '.scss']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
         }
-    },
-    plugins: [
-        new MiniCssExtractPlugin({filename: "[name].css"}),
-        new HtmlWebPackPlugin({template: "./src/index.html"})
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/i,
+        exclude: /node_modules/,
+        loader: 'file-loader?name=[name].[hash].[ext]'
+      }
     ]
-};
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          chunks: 'initial',
+          name: 'vendor',
+          test: /node_modules/,
+          enforce: true
+        }
+      }
+    },
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: '[name].css' }),
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      alwaysWriteToDisk: true
+    }),
+    new HtmlWebpackHarddiskPlugin()
+  ]
+}
