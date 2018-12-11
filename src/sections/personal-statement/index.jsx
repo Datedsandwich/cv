a65cc08c2bfe0f9a1a52471c1d15328fb38999e5
Luke Robinson <robinsonlm@live.co.uk>
2018-12-11 21:45:32 +0000

    Updated personal statement

--- src/sections/personal-statement/index.jsx
+++ src/sections/personal-statement/index.jsx
@@ -6,18 +6,17 @@ export const PersonalStatement = () => (
   <Section title="Hello!">
     <p className="lr-u-soft-bottom">
       I am a Software Developer and Game Developer. I've worked as a Gameplay
-      Programmer, and also as a Full Stack Web Developer. I've used C#, Java,
-      and JavaScript, alongside Unity, XNA/MonoGame, Angularjs, React, and
-      Dropwizard.
+      Programmer, and also as a Full Stack Web Developer, on a variety of
+      projects across a multitude of technologies.
     </p>
     <p className="lr-u-soft-bottom">
-      I am currently seeking a role in the Games Industry. I have an interest in
-      gameplay programming, but am more than capable on the UI or back-end of a
-      game also.
+      I have a preference for Front-End development, I enjoy the rapid, visual,
+      feedback loop. I also find that modern front-end frameworks are a pleasure
+      to work with.
     </p>
     <p>
-      I've also got some interest in web, as you can see by looking at this
-      page! It's responsive, too!
+      Currently, I'm a big fan of ReactJS and ES6, and have a bit of a
+      reputation in my office for proselytising about GraphQL.
     </p>
   </Section>
 )
