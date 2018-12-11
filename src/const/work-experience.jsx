f56e1d5815b259ac061a46cc95d98ae45acd7383
Luke Robinson <robinsonlm@live.co.uk>
2018-12-11 21:16:23 +0000

    Updated work experience

--- src/const/work-experience.jsx
+++ src/const/work-experience.jsx
@@ -1,21 +1,52 @@
+import React from 'react'
+
 export const workExperience = [
   {
     key: 1,
-    timeframe: 'August 2016 - March 2018',
+    timeframe: 'August 2018 - Current',
     title: 'BlackCat Technology Solutions',
     subtitle: 'Software Engineer - Birmingham',
-    body:
-      'Worked on a Fixed Asset Management Suite, using Java (and Dropwizard) for the ba' +
-      'ck-end, and JavaScript (AngularJS) for the Front-End.'
+    body: (
+      <p>
+        Working on an Ethics and Compliance tool, using Java (and Spring Boot)
+        for the backend and React/Redux for the front-end.
+      </p>
+    )
   },
   {
     key: 2,
+    timeframe: 'June 2018 - July 2018',
+    title: 'Bite the Ballot',
+    subtitle: 'Software Engineer - London',
+    body: (
+      <p>
+        Worked on Verto, a React front-end with a Node.js and GraphQL back-end.
+      </p>
+    )
+  },
+  {
+    key: 3,
+    timeframe: 'August 2016 - March 2018',
+    title: 'BlackCat Technology Solutions',
+    subtitle: 'Software Engineer - Birmingham',
+    body: (
+      <p>
+        Worked on a Fixed Asset Management Suite, using Java (and Dropwizard)
+        for the ck-end, and JavaScript (AngularJS) for the Front-End.
+      </p>
+    )
+  },
+  {
+    key: 4,
     timeframe: 'November 2015 - August 2016',
     title: 'Invisibit Studios',
     subtitle: 'Gameplay Programmer - Great Malvern',
-    body:
-      'Working on Bip, written in C# using the Unity engine, and myself and the Technic' +
-      'al Lead would often engage in Pair Programming to ensure the quality of the code' +
-      ' we produced.'
+    body: (
+      <p>
+        Working on Bip, written in C# using the Unity engine, and myself and the
+        Technical Lead would often engage in Pair Programming to ensure the
+        quality of the code we produced.
+      </p>
+    )
   }
 ]
